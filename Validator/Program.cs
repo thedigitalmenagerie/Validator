8ef082e233b4973529ab05985e4828fc0073d6cd
thedigitalmenagerie <honeyraeswan@gmail.com>
2021-07-28 19:05:06 -0400

    first commit

---
+++ Validator/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+
+namespace Validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a pin of only digits, numbering 4-8 characters:");
+            var userPinInput = Console.ReadLine();
+            if (userPinInput.All(char.IsDigit) && userPinInput.Length >= 4 && userPinInput.Length <= 8)
+            {
+                var validPin = true;
+                if (validPin == true)
+                {
+                    Console.WriteLine("Your pin is valid");
+                }
+            } 
+            else
+            {
+                var validPin = false;
+                if (validPin == false)
+                {
+                    Console.WriteLine("Your pin is not valid");
+                }
+            }
+
+            Console.WriteLine("Please enter your phone number");
+            var userEnteredPhone = Console.ReadLine();
+            var digitsOnly = string.Join("", userEnteredPhone.Split('-', '(', ')', ' '));
+            if (digitsOnly.Length != 10)
+            {
+                var validPhone = false;
+                if (validPhone == false)
+                {
+                    Console.WriteLine("That is not a valid phone number");
+                }
+            }
+            else if (digitsOnly.StartsWith("555"))
+            {
+                var validPhone = false;
+                if (validPhone == false)
+                {
+                    Console.WriteLine("That is not a valid phone number");
+                }
+            }
+            else if (digitsOnly.All(char.IsDigit))
+            {
+                var validPhone = true;
+                if (validPhone == true)
+                {
+                    Console.WriteLine("That is a valid phone number");
+                }
+            }
+            else
+            {
+                var validPhone = false;
+                if (validPhone == false)
+                {
+                    Console.WriteLine("That is not a valid phone number");
+                }
+            }
+        }
+
+    }
+}
